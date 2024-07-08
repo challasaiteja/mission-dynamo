ec6fe711265a7ea44dad6959fc68f400de807aab
challasaiteja <saitejachalla2001@gmail.com>
2024-07-08 11:04:45 -0400

    mission-dynamo

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+from fastapi.middleware.cors import CORSMiddleware
+from services.genai import YoutubeProcessor, GeminiProcessor
+
+class VideoAnalysisRequest(BaseModel):
+    youtube_link: HttpUrl
+    #advanced settings
+
+app = FastAPI()
+
+#Configuring CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+genai_processor= GeminiProcessor(
+        model_name= "gemini-pro",
+        project= "mission-dynamo-428115"  
+    )
+
+@app.post("/analyze_video")
+def analyze_video(request: VideoAnalysisRequest):
+    processor= YoutubeProcessor(genai_processor= genai_processor)
+    result= processor.retrieve_youtube_documents(str(request.youtube_link), verbose= True)
+
+    #summary= genai_processor.generate_document_summary(result, verbose= True)
+    key_concepts= processor.find_key_concepts(result, verbose= True)
+
+    return {
+        "key_concepts": key_concepts
+    }
